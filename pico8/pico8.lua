0c2c6aa4596b429410fcdaaa2efba396e08105df
pancelor <pancelor@gmail.com>
2025-01-20 10:18:40 -0800

    pico8: fix update/draw

--- pico8/pico8.lua
+++ pico8/pico8.lua
@@ -2,12 +2,24 @@
 
 -- Main Loop
 
----called once just before the main loop
+---Called once at startup, just before the main loop.
+---
+---[View Online](https://www.lexaloffle.com/dl/docs/pico-8_manual.html#PICO_8_Program_Structure)
 function _init() end
 
----called 60 times per second
+---Called 30 times per second.
+---Define _update60() instead if you want 60FPS
+---
+---[View Online](https://www.lexaloffle.com/dl/docs/pico-8_manual.html#PICO_8_Program_Structure)
 function _update() end
 
----called each time the window manager asks for a frame
----normally 60, 30 or 20 times per second
+---Called 60 times per second.
+---
+---[View Online](https://www.lexaloffle.com/dl/docs/pico-8_manual.html#_UPDATE60)
+function _update60() end
+
+---Called each time the window manager asks for a frame.
+---normally 60, 30 or 15 times per second
+---
+---[View Online](https://www.lexaloffle.com/dl/docs/pico-8_manual.html#PICO_8_Program_Structure)
 function _draw() end
